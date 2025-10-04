56cb6b9837fc485beda76963e837daf592dba8b4
AlbaHeredia <albaheredia1902@gmail.com>
2025-10-04 15:28:44 -0300

    Update CargaMensual.java
    ;)

--- CargaMensual.java
+++ CargaMensual.java
@@ -2,8 +2,14 @@ public class CargaMensual {
     private int numero;
     private String fechaYHora;
     private double importe;
+    private String mesVigencia;
 
-    public CargaMensual() {}
+    public CargaMensual(int número, String fechaYhora, double importe, String mesVigencia){
+        this.número = número;
+        this.fechaYhora = fechaYhora;
+        this.importe = importe;
+        this.mesVigencia = mesVigencia;
+    }
 
     // Getters
     public int getNumero() {
